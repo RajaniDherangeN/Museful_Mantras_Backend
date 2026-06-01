69e9509ddfe4baa06a01b4d02b8de239d7f133ad
Rajani Dherange <133220243+RajaniDherangeN@users.noreply.github.com>
2026-06-01 19:24:17 +0530

    Update HomeController.java

--- src/main/java/com/Art_Portfolio/art_portfolio/controller/HomeController.java
+++ src/main/java/com/Art_Portfolio/art_portfolio/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	 @GetMapping("/")
-	    public String home() {
-	        return "index";  // this refers to templates/index.html
-	    }
+    @GetMapping("/")
+    public String home() {
+        return "API is running!";
+    }
 }
